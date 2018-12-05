62705c6af7f20b93ca3a7bc4730081902265ec21
Johan Björk <johanimon@gmail.com>
2018-12-05 04:22:05 +0100

    Formatting

--- src/throttle.ts
+++ src/throttle.ts
@@ -2,14 +2,16 @@ import {VariadicFunc} from "./types";
 
 export function throttle<T extends any[]>(func: VariadicFunc<T, any>, wait: number): VariadicFunc<T, void> {
     let timeout: number | undefined
-    return function(this: any, ...args: T) {
+    return function (this: any, ...args: T) {
         const context = this
+
         function later() {
             timeout = undefined
             func.apply(context, args)
         }
+
         if (typeof timeout === 'undefined') {
             timeout = setTimeout(later, wait)
         }
-    } as any
+    }
 }
