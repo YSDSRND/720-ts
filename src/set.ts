020ca29a95069b434bcbe13c1ee4478b67bb357f
Johan Björk <johanimon@gmail.com>
2019-01-09 13:56:40 +0100

    Implement set()

---
+++ src/set.ts
@@ -0,0 +1,15 @@
+export function set(item: any, path: string, value: any): void {
+    const parts = path.split('.')
+    let slice = item
+    // we want to use the last part of the path
+    // to put the value into place, so let's keep
+    // one item in the parts array.
+    while (parts.length > 1) {
+        const part = parts.shift()!
+        if (!Object.prototype.hasOwnProperty.call(slice, part)) {
+            slice[part] = {}
+        }
+        slice = slice[part]
+    }
+    slice[parts[0]] = value
+}
---
+++ tests/set.ts
@@ -0,0 +1,35 @@
+import {set} from "../src/set";
+
+describe('set tests', function () {
+
+    it('should set with shallow path', function () {
+        const item: any = {
+            yee: undefined,
+        }
+        set(item, 'yee', 1)
+        expect(item.yee).toBe(1)
+    })
+
+    it('should set with deep path', function () {
+        const item: any = {
+            yee: {
+                boi: undefined,
+            }
+        }
+        set(item, 'yee.boi', 1)
+        expect(item.yee.boi).toBe(1)
+    })
+
+    it('should create objects for paths that do not exist', function () {
+        const item = {}
+        set(item, 'yee.boi.cool', 1)
+        expect(item).toEqual({
+            yee: {
+                boi: {
+                    cool: 1,
+                },
+            },
+        })
+    })
+
+})
