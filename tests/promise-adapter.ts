5f8a542a539ce25086c8dd0c2dc9305b2ea6a596
Johan Björk <johanimon@gmail.com>
2019-03-10 13:56:41 +0100

    Fix broken test adapter for promise

--- tests/promise-adapter.ts
+++ tests/promise-adapter.ts
@@ -1,7 +1,15 @@
 import {Promise} from "../src/promise";
 
-export const resolved = Promise.resolve
-export const rejected = Promise.reject
+export const resolved = function(value: any) {
+    return new Promise((resolve, reject) => {
+        resolve(value)
+    })
+}
+export const rejected = function(value: any) {
+    return new Promise((resolve, reject) => {
+        reject(value)
+    })
+}
 export function deferred() {
     let a, b
     let promise = new Promise((resolve, reject) => {
