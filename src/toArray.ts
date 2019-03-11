8343f247a71f01c7a931421fc4633e488795b2b3
Johan Björk <johanimon@gmail.com>
2019-03-11 03:27:14 +0100

    Improve toArray() performance

--- src/toArray.ts
+++ src/toArray.ts
@@ -1,7 +1,3 @@
 export function toArray<T>(items: ArrayLike<T>): ReadonlyArray<T> {
-    const arrayed = new Array(items.length)
-    for (let i = 0; i < items.length; i++) {
-        arrayed[i] = items[i]
-    }
-    return arrayed
+    return Array.prototype.slice.call(items)
 }
